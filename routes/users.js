c0bf28fd20e15aed2932d0a8de89004938c1ca81
pavel <pavel/ps2014@yandex.ru>
2021-01-18 14:25:26 +0300

    Добавил

--- routes/users.js
+++ routes/users.js
@@ -1,17 +1,18 @@
 const routerUsers = require('express').Router();
 const { celebrate, Joi } = require('celebrate');
 const {
-  findUser, getUserById, patchUser, patchUserAvatar,
+  findUser, getUserById, createUser, patchUser, patchUserAvatar,
 } = require('../controllers/users');
 
 routerUsers.get('/users', findUser);
-routerUsers.get('/users/:id', celebrate({
+routerUsers.get('/users/:id',celebrate({
   params: Joi.object().keys({
     id: Joi.string().hex().required().length(24),
   }),
 }), getUserById);
 
 
+routerUsers.post('/users', createUser);
 routerUsers.patch('/users/me', celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
@@ -22,6 +23,6 @@ routerUsers.patch('/users/me/avatar', celebrate({
   body: Joi.object().keys({
     avatar: Joi.string().required().pattern(/^(https?:\/\/(www\.)?)[\w-]+\.[\w./():,-]+#?$/),
   }),
-}), patchUserAvatar);
+}),patchUserAvatar);
 
 module.exports = routerUsers;
